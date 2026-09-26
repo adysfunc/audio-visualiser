32b0e85f215ee32d6f3ed964977c637b86b6f829
adysfunc <ayeshaimama8@gmail.com>
2026-09-26 22:03:33 +0530

    added comments and changed the way to play music

--- display.py
+++ display.py
@@ -1,5 +1,4 @@
 import pygame
-import pygame.sndarray
 import numpy as np
 import cmath
 import wave
@@ -13,8 +12,9 @@ def fft(samps):
     else:
         evensamps = fft(samps[::2])
         oddsamps = fft(samps[1::2])
-        # freq = array.array('i', [])
+    
         # after spilliting to a single sample
+        # the FFT calculation 
         for k in range(samplen//2):
             twiddle = cmath.exp(-2j*cmath.pi*k/samplen)
             freq[k] = evensamps[k] + twiddle*oddsamps[k]
@@ -23,47 +23,30 @@ def fft(samps):
 
 def read():
     with wave.open('voice-telephony-8khz.wav') as wv_obj:
-        # samples = wv_obj.readframes(100)
         FRAMERATE = wv_obj.getframerate()
         samples = wv_obj.readframes(wv_obj.getnframes())
-        # print(wv_obj.getparams())
-        # print(wv_obj.getnframes()/wv_obj.getframerate())
-    # int_samples = array.array('h', samples).tolist()
-    # padded_samps = int_samples.extend([0]*51744)
+     
+     #convert raw bytes to signed integers then split them into arrays of 512 samples(used the array module previously)   
     int_samples = np.frombuffer(samples, dtype='h') 
     chunks =  np.array_split(int_samples, np.arange(512, len(int_samples), 512))
     # print(len(chunks))
     # # print(chunks)
     # print((chunks[0]))
+    
     mags = []
     for i in range(len(chunks)):
         mags.append(np.abs(fft(chunks[i])))
-    # print(mags[0][:256])
-    # print(len(mags))
-    # print(mags)
-    # print(int_samples) 
-    # padded_samps = np.pad(int_samples, (0, 51744), mode='constant')
-    # print(padded_samps[-1])
     
-    # freqs = fft(chunks)
-    # nparr = np.array(freqs, dtype=complex)
-    # mags = np.abs(freqs)
-    # print(mags.size)
     #since real samples produce the same magnitudes in the negative and postive frequency bins
     # we can discard the values in the negative bins and instead multiply the magnitudes in the positive side(excluding the 1st bin(oth bin) and the last Nyquist(256thbin))
     # for now just remving the 0th and the 256th bin and in this code
     positive_spectrum = [mag[:mag.size//2+1][1:-1]*2 for mag in mags]
-    # print(positive_sepctrum[0])
-    # positive_sepctrum = mags[:mags.size//2+1]
-    # positive_sepctrum[1:-1] *= 2
-    # freq_res = wv_obj.getframerate()/wv_obj.getnframes() 
+     
     freq_res = FRAMERATE/512
-    freq_bins = [np.arange(len(values))* freq_res for values in positive_spectrum]
+    #no need of frequency bins to plot only did it to understand the FFT
+    # freq_bins = [np.arange(len(values))* freq_res for values in positive_spectrum]
     return positive_spectrum, freq_res
-    # print(len(freq_bins[0]), len(positive_spectrum[0]))
-    # actual_freqbins = np.arange(len(positive_spectrum)) * freq_res
-    # print(type(freq_bins), type(positive_spectrum[0]))
-
+    
 points, frequency_resolution = read()
 delta = frequency_resolution/60
 pygame.mixer.pre_init(frequency = 8000, size= -16, channels = 1, buffer = 512)
@@ -72,6 +55,8 @@ pygame.mixer.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+#used the msuic module instead of sound
+pygame.mixer.music.load('voice-telephony-8khz.wav')
 
 count = 0
 while running:
@@ -82,26 +67,26 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     # bin_to_draw = points[0]
+    # to choose which chunk to show
     count += delta
-    bin_to_draw = points[int(count)]
+    frame_to_draw = points[int(count)]
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 
     # RENDER YOUR GAME HERE
-    sound = pygame.mixer.Sound('voice-telephony-8khz.wav')
-    sound.play()
-    for i in range(len(bin_to_draw)):
-        maxmag = max(bin_to_draw)
-        bar_width = screen.get_width()/len(bin_to_draw)
-        height = bin_to_draw[i]/maxmag * 720
+    for i in range(len(frame_to_draw)):
+        #get the max value from the frame samples
+        maxmag = max(frame_to_draw)
+        bar_width = screen.get_width()/len(frame_to_draw)
+        #for every sample/point divide bt the max value and multiply by the screen width in order to fit them in the screen
+        height = frame_to_draw[i]/maxmag * 720
         pygame.draw.rect(screen, (255,255,255), (i*bar_width, 720-height, bar_width, height))
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+
+    if not pygame.mixer.music.get_busy():
+        pygame.mixer.music.play()
+    
     clock.tick(60)  # limits FPS to 60
+    pygame.display.update()
 
 pygame.quit()
 pygame.mixer.quit()
-
-# sound = pygame.mixer.Sound('voice-telephony-8khz.wav')
-# samples = pygame.sndarray.samples(sound)
-# print(samples)
